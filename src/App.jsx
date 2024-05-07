f046430df5943225aed49b52cebd4fe12ca7a6f7
neddyKG <gonzalezneddy@gmail.com>
2024-05-07 18:25:47 -0400

    Changed to navLink, fixed color change on active and added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
+import React from "react";
 import {
   Route,
+  RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider,
 } from "react-router-dom";
-import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import AllJobsButton from "./components/AllJobsButton";
-import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />}/>
+      <Route path="*" element={<NotFoundPage />}/>
     </Route>
   )
 );
