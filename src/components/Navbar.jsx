401a133989c90a6ba933476b352c06a553cd4e46
neddyKG <gonzalezneddy@gmail.com>
2024-05-07 18:45:13 -0400

    Added jobs page and refactored JobListings for showing only 3 for home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 import React from "react";
-import logo  from '../assets/images/logo.png'
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
-
   const linkClass = ({ isActive }) => {
-    let navLinkStyle = ''
-    isActive ? navLinkStyle = 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' :
-    navLinkStyle = 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-    return navLinkStyle
-  }
+    let navLinkStyle = "";
+    isActive
+      ? (navLinkStyle =
+          "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2")
+      : (navLinkStyle =
+          "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2");
+    return navLinkStyle;
+  };
 
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -17,37 +19,21 @@ const Navbar = () => {
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
-            <NavLink
-              className="flex flex-shrink-0 items-center mr-4"
-              to="/"
-            >
-              <img
-                className="h-10 w-auto"
-                src={logo}
-                alt="React Jobs"
-              />
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 React Jobs
               </span>
             </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <NavLink
-                  to="/"
-                  className={linkClass}
-                >
+                <NavLink to="/" className={linkClass}>
                   Home
                 </NavLink>
-                <NavLink
-                  to="/jobs"
-                  className={linkClass}
-                >
+                <NavLink to="/jobs" className={linkClass}>
                   Jobs
                 </NavLink>
-                <NavLink
-                  to="/add-job"
-                  className={linkClass}
-                >
+                <NavLink to="/add-job" className={linkClass}>
                   Add Job
                 </NavLink>
               </div>
