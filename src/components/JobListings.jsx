6ce64e0c381a756391d9e74d3de1ac6c12593d4a
neddyKG <gonzalezneddy@gmail.com>
2024-05-08 11:22:00 -0400

    Added a json-server as the backend and implemented a GET request for jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,19 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import JobCard from "./JobCard";
 import jobs from "../jobs.json";
 
 const JobListings = ({ isHome = false }) => {
-  const jobList = isHome ? jobs.slice(0, 3) : jobs
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //   You want to have the empty array, cause if not you'll have an infinite loop
+  useEffect(() => {
+    const getJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching jobs");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          { isHome ? 'Recent Jobs' : 'Browse Jobs' }
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobList.map((job) => (
-            <JobCard key={job.id} job={job} />
-          ))}
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : (
+            jobs.map((job) => <JobCard key={job.id} job={job} />)
+          )}
         </div>
       </div>
     </section>
